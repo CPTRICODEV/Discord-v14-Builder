1aa82519a392fc8dc86cb2b91bf8120b803c3969
Patrick Jakobsen <97399891+CPTRICODEV@users.noreply.github.com>
2023-07-11 23:37:55 +0200

    Fixed Admin Folder

--- templates/DiscordJS/Commands/Admin/admin.js
+++ templates/DiscordJS/Commands/Admin/admin.js
@@ -7,7 +7,7 @@ const {
 module.exports = {
   developer: false, // if you have setup a developer guild and you want to check the command out in your developer guild then set this to true!.
   data: new SlashCommandBuilder()
-    .setName("staff")
+    .setName("Admin")
     .setDescription("Reply to you with Pong!")
     .setDefaultMemberPermissions(PermissionFlagsBits.Administrator), // this will set the permissions of the command, remove it if you want everyone to use it, leave it if you want only administrators to use the command.
 
